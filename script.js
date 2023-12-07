99a1268a8eb338962ec6451405d41ff694d1acf0
KauanAlvz <kauanalves0207@gmail.com>
2023-12-07 12:51:46 -0300

    classificação

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
-function calcImc(){
-    const peso = parseFloat(document.getElementById('peso').value);
+function calcImc() {
     const altura = parseFloat(document.getElementById('altura').value);
-
+    const peso = parseFloat(document.getElementById('peso').value);
     const imc = (peso / (altura * altura)).toFixed(1);
 
-    document.querySelector('#resposta').innerHTML = imc;
+    document.getElementById('resposta').innerHTML = imc + ', ' + classificacao(imc);
+}
+
+function classificacao(imc){
+    if(imc < 18.5){
+        return 'magreza';
+    }
+    if(imc >= 18.5 && imc <= 24.9){
+        return 'normal'; 
+    }
+    if(imc >= 25 && imc <= 29.9){
+        return 'obesidade I'; 
+    }
+    if(imc >= 30 && imc <= 39.9){
+        return 'obesidade II'; 
+    }
+    if(imc >= 40){
+        return 'obesidade III'; 
+    }
 }
